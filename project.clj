6994f1a3202e225c70a5a6b5e4940a4fe6063d94
Brian Edwards <brian@edwardstx.us>
2018-05-17 15:18:49 -0700

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject io.hosaka/user "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot io.hosaka.user
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
